581c96d75fef34b77857d399b96a3793fbbc9217
DIYA <diya94816@gmail.com>
2025-02-27 23:15:56 +0530

    committed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App=()=>{
+  return(
+    <div style={{textAlign:"center", padding:"50px"}}>
+      <h1>Welcome </h1>
+      <p>My name is Diya</p>
+      <p> contact number : 7970322994</p>
+    </div>
+  );
+};
+export default App;
